7d965baa971655559ddb8dd5635a5083d498a429
dwbaek <bdw7475@gmail.com>
2024-03-28 15:57:54 +0900

    41. output dynamic component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-function Header() { //1. 컴포넌트명은 대문자 
-  return( //2. 괄호로 묶어 이 코드가 함께 있다는 것을 표시해야 한다. 
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+// 무작위로 숫자를 출력하는 함수
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)]
+  
+  return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {/* 중괄호 안의 내용에 리턴값이 바로 나오는 자바스크립트 코드를 자유롭게 추가 가능 */}
+        {description} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
@@ -14,7 +23,6 @@ function Header() { //1. 컴포넌트명은 대문자
 function App() {
   return (
     <div>
-      {/* 3. HTML요소를 사용하는것과 같이 리액트 컴포넌트를 사용하면 된다.  */}
       <Header />
       <main>
         <h2>Time to get started!</h2>
--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,4 @@ import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(<App />); // entryPoint 안에 <App/> 컴포넌트를 렌더링 
